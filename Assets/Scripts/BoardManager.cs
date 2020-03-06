8cb38a1ee8340a1887c7e01000a70fa43d8a5273
Silvia Zambrana <silviazambrana3@gmail.com>
2020-03-06 19:29:54 +0100

    Initial commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+
+    public List<GameObject> floors;
+    public List<GameObject> walls;
+    public List<GameObject> outerwalls;
+    public List<GameObject> food;
+    public List<GameObject> soda;
+    public GameObject exit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
